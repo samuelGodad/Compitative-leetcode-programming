18b7aca5ddebd2b8ed827fd82699a0a07c742d5a
samuelGodad <117444585+samuelGodad@users.noreply.github.com>
2023-01-17 00:42:49 +0300

    Update kthLargestNumber.py

--- kthLargestNumber.py
+++ kthLargestNumber.py
@@ -3,3 +3,13 @@
 # Return the string that represents the kth largest integer in nums.
 
 # Note: Duplicate numbers should be counted distinctly. For example, if nums is ["1","2","2"], "2" is the first largest integer, "2" is the second-largest integer, and "1" is the third-largest integer.
+def arr(nums,k):
+    for i in range(len(nums)):
+        mins=i
+        for j in range(i+1,len(nums)):
+            if int(nums[mins])<=int(nums[j]):
+                mins=j
+        (nums[mins],nums[i])=(nums[i],nums[mins])
+    return nums[k-1]
+k=3  
+print(arr(["2","21","12","1"],k))
