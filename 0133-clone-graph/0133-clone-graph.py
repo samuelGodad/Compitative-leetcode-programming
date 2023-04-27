dbe626d5c6a80edd8d6f69ecfc34b0d92213d6f0
samuelGodad <117444585+samuelGodad@users.noreply.github.com>
2023-04-27 17:46:27 +0300

    Time: 46 ms (14.97%), Space: 16.7 MB (15.85%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -6,22 +6,42 @@ class Node:
         self.neighbors = neighbors if neighbors is not None else []
 """
 
-class Solution:
-    def __init__(self):
-        self.visited = {}
+# class Solution:
+#     def __init__(self):
+#         self.visited = {}
 
+#     def cloneGraph(self, node: 'Node') -> 'Node':
+#         if not node:
+#             return node
+
+#         if node in self.visited:
+#             return self.visited[node]
+
+#         clone_node = Node(node.val, [])
+#         self.visited[node] = clone_node
+
+#         if node.neighbors:
+#             clone_node.neighbors = [self.cloneGraph(n) for n in node.neighbors]
+
+#         return clone_node
+class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
         if not node:
             return node
-
-        if node in self.visited:
-            return self.visited[node]
-
+        
+        visited = {}
+        queue = [node]
         clone_node = Node(node.val, [])
-        self.visited[node] = clone_node
-
-        if node.neighbors:
-            clone_node.neighbors = [self.cloneGraph(n) for n in node.neighbors]
-
+        visited[node] = clone_node
+        
+        while queue:
+            curr_node = queue.pop(0)
+            for neighbor in curr_node.neighbors:
+                if neighbor not in visited:
+                    visited[neighbor] = Node(neighbor.val, [])
+                    queue.append(neighbor)
+                visited[curr_node].neighbors.append(visited[neighbor])
+        
         return clone_node
+
         
