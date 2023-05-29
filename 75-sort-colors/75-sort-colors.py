d83e7aad3acb561954b0679003a76e8230e71339
samuelGodad <117444585+samuelGodad@users.noreply.github.com>
2023-05-29 19:30:52 +0300

    Time: 48 ms (35.84%), Space: 16.4 MB (11.13%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -7,20 +7,20 @@ class Solution:
         low, mid, high = 0, 0, len(nums) - 1
         n=len(nums)
 
-#         while mid <= high:
-#             if nums[mid] == 0:
-#                 nums[mid], nums[low] = nums[low], nums[mid]
-#                 low += 1
-#                 mid += 1
-#             elif nums[mid] == 1:
-#                 mid += 1
-#             elif nums[mid] == 2:
-#                 nums[mid], nums[high] = nums[high], nums[mid]
-#                 high -= 1
-        for i in range(n):
-            for j in range(i+1,n):
-                if nums[i]>nums[j]:
-                    nums[i],nums[j]=  nums[j],nums[i]
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[mid], nums[low] = nums[low], nums[mid]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            elif nums[mid] == 2:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
+#         for i in range(n):
+#             for j in range(i+1,n):
+#                 if nums[i]>nums[j]:
+#                     nums[i],nums[j]=  nums[j],nums[i]
         
 
         return nums
