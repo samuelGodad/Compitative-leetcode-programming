4331d1c7bf1ac87862ac7ae5dbbafb4d6f4e1bbd
samuelGodad <117444585+samuelGodad@users.noreply.github.com>
2023-07-01 17:41:39 +0300

    Time: 254 ms (86.55%), Space: 29.1 MB (39.97%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
@@ -4,9 +4,8 @@ class Solution:
         left = 0
         right = 0
         n = len(nums)
-        min_length = float('inf')
+        min_length =n+1
         sum = 0
-
         while right < n:
             sum += nums[right]
             while sum >= target:
@@ -16,6 +15,6 @@ class Solution:
             right += 1
 
       
-        if min_length != float('inf'):
+        if min_length != n+1:
             return min_length
         return 0
